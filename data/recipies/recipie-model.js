8b5b0c19f457eedf5f309a417780745d3e882b49
abarne <celius377@gmail.com>
2019-11-07 16:00:35 -0600

    all helper functions and endpoints complete

--- data/recipies/recipie-model.js
+++ data/recipies/recipie-model.js
@@ -3,13 +3,33 @@ const db = require('../db-config');
 module.exports = {
 	getRecipes,
 	getShoppingList,
-	getInstructions
+	getInstructions,
+	getRecipiesByIngredient
 };
 
 function getRecipes() {
 	return db('Recipie');
 }
 
-function getShoppingList(recipe_id) {}
+function getShoppingList(recipe_id) {
+	return db
+		.select('Quantity')
+		.from('Recipie_Ingredient')
+		.select('Name')
+		.from('Ingredients')
+		.join('Recipie_Ingredient', 'Ingredients.id', '=', 'Recipie_Ingredient.Ingredients_Id')
+		.where('Recipie_Ingredient.Recipie_Id', recipe_id);
+}
 
-function getInstructions(recipe_id) {}
+function getInstructions(recipe_id) {
+	return db.select('Instructions').from('Recipie').where('Recipie.id', recipe_id);
+}
+
+function getRecipiesByIngredient(ingredient) {
+	return db
+		.select('Recipie.Name', 'Recipie.Instructions')
+		.from('Recipie')
+		.join('Recipie_Ingredient', 'Recipie_Ingredient.Recipie_id', '=', 'Recipie.id')
+		.join('Ingredients', 'Recipie_Ingredient.Ingredients_Id', '=', 'Ingredients.id')
+		.where('Ingredients.id', ingredient);
+}
